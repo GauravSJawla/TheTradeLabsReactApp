043e00d06f612d40920af6f3dd7b4a8b11535f3c
GauravSJawla <gauravsinghjawla@gmail.com>
2020-02-09 18:39:30 -0600

    Security Patch 2.0 Github API Fix

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -14,10 +14,9 @@ const Post = require('../../models/Post');
 // @access  Private
 router.get('/me', auth, async (req, res) => {
   try {
-    const profile = await Profile.findOne({ user: req.user.id }).populate(
-      'user',
-      ['name', 'avatar']
-    );
+    const profile = await Profile.findOne({
+      user: req.user.id
+    }).populate('user', ['name', 'avatar']);
 
     if (!profile) {
       return res.status(400).json({ msg: 'There is no profile for this user' });
@@ -352,13 +351,14 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 router.get('/github/:username', (req, res) => {
   try {
     const options = {
-      uri: `https://api.github.com/users/${
-        req.params.username
-      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
-        'githubClientId'
-      )}&client_secret=${config.get('githubSecret')}`,
+      uri: encodeURI(
+        `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+      ),
       method: 'GET',
-      headers: { 'user-agent': 'node.js' }
+      headers: {
+        'user-agent': 'node.js',
+        Authorization: `token ${config.get('githubToken')}`
+      }
     };
 
     request(options, (error, response, body) => {
